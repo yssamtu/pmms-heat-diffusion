d2ea6ba29296e4896760f1d004753e0d6af94d50
Yung-sheng Tu <yssamtu@gmail.com>
2023-03-13 19:10:49 +0100

    set limmit

--- assignment_3/pipesort/pipesort.c
+++ assignment_3/pipesort/pipesort.c
@@ -1,46 +1,44 @@
-#include <stdbool.h>
+#include <errno.h>
+#include <limits.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 #include <pthread.h>
+#include <sys/resource.h>
 #include <unistd.h>
-#include <errno.h>
-
-typedef struct {
-    int *message;
-    int *output;
-    pthread_mutex_t *message_lock;
-    pthread_mutex_t *output_lock;
-    pthread_cond_t *message_item;
-    pthread_cond_t *message_space;
-    pthread_cond_t *output_item;
-    pthread_cond_t *output_space;
-} profile_t;
-
-typedef struct {
-    int *message;
-    pthread_mutex_t *message_lock;
-    pthread_cond_t *message_item;
-    pthread_cond_t *message_space;
-} output_t;
 
 enum {
     CONSUMED = -1,
     END = -2,
-    BUF_SIZE = 4088
+    BUF_SIZE = 1
 };
 
-static inline void next_message(int **now, int *head);
-static inline void next_lock(pthread_mutex_t **now, pthread_mutex_t *head);
-static void *comparator(void *profile);
+typedef struct {
+    int *buf;
+    pthread_mutex_t *lock;
+    pthread_cond_t *item;
+    pthread_cond_t *space;
+} buf_set_t;
+
+typedef struct {
+    buf_set_t input;
+    buf_set_t output;
+} inout_set_t;
+
+static void *comparator(void *inout_set);
 static void *successor(void *output);
 
 int main(int argc, char *argv[])
 {
-    int c;
+    // Set limit of the number of threads
+    struct rlimit rl = {0};
+    getrlimit(RLIMIT_NPROC, &rl);
+    rl.rlim_cur = rl.rlim_max;
+    setrlimit(RLIMIT_NPROC, &rl);
+    // Argument
     unsigned seed = 42;
     long length = 1e4;
-    while((c = getopt(argc, argv, "l:s:")) != -1) {
+    for (int c = getopt(argc, argv, "l:s:"); c != -1; c = getopt(argc, argv, "l:s:")) {
         switch(c) {
             case 'l':
                 length = atol(optarg);
@@ -56,210 +54,237 @@ int main(int argc, char *argv[])
         }
     }
     srand(seed);
-    int message[(length + 1) * BUF_SIZE];
+    // Thread setting
+    int buf[(length + 1) * BUF_SIZE];
     pthread_mutex_t lock[(length + 1) * BUF_SIZE];
     pthread_cond_t item[length + 1];
     pthread_cond_t space[length + 1];
-    for (long i = 0; i < BUF_SIZE * (length + 1); ++i) {
-        message[i] = CONSUMED;
+    for (long i = 0; i < (length + 1) * BUF_SIZE; ++i) {
+        buf[i] = CONSUMED;
         pthread_mutex_init(&lock[i], NULL);
     }
     for (long i = 0; i < length + 1; ++i) {
         pthread_cond_init(&item[i], NULL);
         pthread_cond_init(&space[i], NULL);
     }
-    int (*message_block)[length + 1][BUF_SIZE] = (int (*)[length + 1][BUF_SIZE])message;
+    int (*buf_block)[length + 1][BUF_SIZE] = (int (*)[length + 1][BUF_SIZE])buf;
     pthread_mutex_t (*lock_block)[length + 1][BUF_SIZE] = (pthread_mutex_t (*)[length + 1][BUF_SIZE])lock;
-    profile_t profile[length];
+    inout_set_t inout_set[length];
     for (long i = 0; i < length; ++i) {
-        profile[i].message = (*message_block)[i];
-        profile[i].output = (*message_block)[i + 1];
-        profile[i].message_lock = (*lock_block)[i];
-        profile[i].output_lock = (*lock_block)[i + 1];
-        profile[i].message_item = &item[i];
-        profile[i].message_space = &space[i];
-        profile[i].output_item = &item[i + 1];
-        profile[i].output_space = &space[i + 1];
+        inout_set[i].input.buf = (*buf_block)[i];
+        inout_set[i].input.lock = (*lock_block)[i];
+        inout_set[i].input.item = &item[i];
+        inout_set[i].input.space = &space[i];
+        inout_set[i].output.buf = (*buf_block)[i + 1];
+        inout_set[i].output.lock = (*lock_block)[i + 1];
+        inout_set[i].output.item = &item[i + 1];
+        inout_set[i].output.space = &space[i + 1];
     }
-    output_t output = {
-        .message = (*message_block)[length],
-        .message_lock = (*lock_block)[length],
-        .message_item = &item[length],
-        .message_space = &space[length]
+    buf_set_t buf_set = {
+        .buf = (*buf_block)[length],
+        .lock = (*lock_block)[length],
+        .item = &item[length],
+        .space = &space[length]
     };
-    pthread_t cal_thread[length];
-    for (long i = 0; i < length; ++i)
-        if (pthread_create(&cal_thread[i], NULL, comparator, &profile[i]))
-            perror("");
-    pthread_t output_thread;
-    if (pthread_create(&output_thread, NULL, successor, &output))
-        perror("");
+    pthread_attr_t attr;
+    pthread_attr_init(&attr);
+    pthread_attr_setstacksize(&attr, PTHREAD_STACK_MIN);
+    pthread_t comparator_thread[length];
+    pthread_t successor_thread;
+    // Create threads
+    for (long i = 0; i < length; ++i) {
+        if (pthread_create(&comparator_thread[i], &attr, comparator, &inout_set[i])) {
+            perror("Comparator");
+            // Destroy attr, mutex and cond
+            pthread_attr_destroy(&attr);
+            for (long i = 0; i < (length + 1) * BUF_SIZE; ++i)
+                pthread_mutex_destroy(&lock[i]);
+            for (long i = 0; i < length + 1; ++i) {
+                pthread_cond_destroy(&item[i]);
+                pthread_cond_destroy(&space[i]);
+            }
+            return 1;
+        }
+    }
+    if (pthread_create(&successor_thread, &attr, successor, &buf_set)) {
+        perror("Successor");
+        // Destroy attr, mutex and cond
+        pthread_attr_destroy(&attr);
+        for (long i = 0; i < (length + 1) * BUF_SIZE; ++i)
+            pthread_mutex_destroy(&lock[i]);
+        for (long i = 0; i < length + 1; ++i) {
+            pthread_cond_destroy(&item[i]);
+            pthread_cond_destroy(&space[i]);
+        }
+        return 1;
+    }
+    int *input = &buf[0];
+    pthread_mutex_t *input_lock = &lock[0];
+    // Calculation start
     struct timespec before = {0};
     clock_gettime(CLOCK_MONOTONIC, &before);
-    int *input = &message[0];
-    pthread_mutex_t *input_lock = &lock[0];
     for (long i = 0; i < length; ++i) {
         pthread_mutex_lock(input_lock);
-        while (*input != CONSUMED)
+        if (*input != CONSUMED)
             pthread_cond_wait(&space[0], input_lock);
         *input = rand();
         pthread_cond_signal(&item[0]);
         pthread_mutex_unlock(input_lock);
-        next_message(&input, &message[0]);
-        next_lock(&input_lock, &lock[0]);
+        input = &buf[0] + (input - &buf[0] + 1) % BUF_SIZE;
+        input_lock = &lock[0] + (input_lock - &lock[0] + 1) % BUF_SIZE;
     }
     for (int i = 0; i < 2; ++i) {
         pthread_mutex_lock(input_lock);
-        while (*input != CONSUMED)
+        if (*input != CONSUMED)
             pthread_cond_wait(&space[0], input_lock);
         *input = END;
         pthread_cond_signal(&item[0]);
         pthread_mutex_unlock(input_lock);
-        next_message(&input, &message[0]);
-        next_lock(&input_lock, &lock[0]);
+        input = &buf[0] + (input - &buf[0] + 1) % BUF_SIZE;
+        input_lock = &lock[0] + (input_lock - &lock[0] + 1) % BUF_SIZE;
     }
     for (long i = 0; i < length; ++i)
-        pthread_join(cal_thread[i], NULL);
-    pthread_join(output_thread, NULL);
+        pthread_join(comparator_thread[i], NULL);
+    pthread_join(successor_thread, NULL);
     struct timespec after = {0};
     clock_gettime(CLOCK_MONOTONIC, &after);
-    for (long i = 0; i < BUF_SIZE * (length + 1); ++i)
+    // Destroy attr, mutex and cond
+    pthread_attr_destroy(&attr);
+    for (long i = 0; i < (length + 1) * BUF_SIZE; ++i)
         pthread_mutex_destroy(&lock[i]);
     for (long i = 0; i < length + 1; ++i) {
         pthread_cond_destroy(&item[i]);
         pthread_cond_destroy(&space[i]);
     }
+    // Print result
     double time = (double)(after.tv_sec - before.tv_sec) +
                   (double)(after.tv_nsec - before.tv_nsec) / 1e9;
     printf("Pipesort took: % .6e seconds \n", time);
     return 0;
 }
 
-static inline void next_message(int **now, int *head)
+static void *comparator(void *inout_set)
 {
-    *now = head + (*now - head + 1) % BUF_SIZE;
-}
-
-static inline void next_lock(pthread_mutex_t **now, pthread_mutex_t *head)
-{
-    *now = head + (*now - head + 1) % BUF_SIZE;
-}
-
-static void *comparator(void *profile)
-{
-    profile_t *pf = profile;
-    int *message = pf->message;
-    int *output = pf->output;
-    pthread_mutex_t *message_lock = pf->message_lock;
-    pthread_mutex_t *output_lock = pf->output_lock;
-    pthread_mutex_lock(message_lock);
-    if (*message == CONSUMED)
-        pthread_cond_wait(pf->message_item, message_lock);
-    int buf = *message;
-    *message = CONSUMED;
-    pthread_cond_signal(pf->message_space);
-    pthread_mutex_unlock(message_lock);
-    next_message(&message, pf->message);
-    next_lock(&message_lock, pf->message_lock);
+    inout_set_t *io_set = inout_set;
+    buf_set_t *in_set = &io_set->input;
+    buf_set_t *out_set = &io_set->output;
+    int *input = in_set->buf;
+    int *output = out_set->buf;
+    pthread_mutex_t *input_lock = in_set->lock;
+    pthread_mutex_t *output_lock = out_set->lock;
+    // First element
+    pthread_mutex_lock(input_lock);
+    if (*input == CONSUMED)
+        pthread_cond_wait(in_set->item, input_lock);
+    int buf = *input;
+    *input = CONSUMED;
+    pthread_cond_signal(in_set->space);
+    pthread_mutex_unlock(input_lock);
+    input = in_set->buf + (input - in_set->buf + 1) % BUF_SIZE;
+    input_lock = in_set->lock + (input_lock - in_set->lock + 1) % BUF_SIZE;
+    // Other elements
     pthread_mutex_lock(output_lock);
     if (*output != CONSUMED)
-        pthread_cond_wait(pf->output_space, output_lock);
-    pthread_mutex_lock(message_lock);
-    if (*message == CONSUMED)
-        pthread_cond_wait(pf->message_item, message_lock);
-    while (*message != END) {
-        if (*message <= buf) {
-            *output = *message;
-            pthread_cond_signal(pf->output_item);
+        pthread_cond_wait(out_set->space, output_lock);
+    pthread_mutex_lock(input_lock);
+    if (*input == CONSUMED)
+        pthread_cond_wait(in_set->item, input_lock);
+    while (*input != END) {
+        if (*input <= buf) {
+            *output = *input;
+            pthread_cond_signal(out_set->item);
             pthread_mutex_unlock(output_lock);
-            *message = CONSUMED;
-            pthread_cond_signal(pf->message_space);
-            pthread_mutex_unlock(message_lock);
+            *input = CONSUMED;
+            pthread_cond_signal(in_set->space);
+            pthread_mutex_unlock(input_lock);
         } else {
             *output = buf;
-            pthread_cond_signal(pf->output_item);
+            pthread_cond_signal(out_set->item);
             pthread_mutex_unlock(output_lock);
-            buf = *message;
-            *message = CONSUMED;
-            pthread_cond_signal(pf->message_space);
-            pthread_mutex_unlock(message_lock);
+            buf = *input;
+            *input = CONSUMED;
+            pthread_cond_signal(in_set->space);
+            pthread_mutex_unlock(input_lock);
         }
-        next_message(&output, pf->output);
-        next_lock(&output_lock, pf->output_lock);
-        next_message(&message, pf->message);
-        next_lock(&message_lock, pf->message_lock);
+        output = out_set->buf + (output - out_set->buf + 1) % BUF_SIZE;
+        output_lock = out_set->lock + (output_lock - out_set->lock + 1) % BUF_SIZE;
+        input = in_set->buf + (input - in_set->buf + 1) % BUF_SIZE;
+        input_lock = in_set->lock + (input_lock - in_set->lock + 1) % BUF_SIZE;
         pthread_mutex_lock(output_lock);
         if (*output != CONSUMED)
-            pthread_cond_wait(pf->output_space, output_lock);
-        pthread_mutex_lock(message_lock);
-        if (*message == CONSUMED)
-            pthread_cond_wait(pf->message_item, message_lock);
+            pthread_cond_wait(out_set->space, output_lock);
+        pthread_mutex_lock(input_lock);
+        if (*input == CONSUMED)
+            pthread_cond_wait(in_set->item, input_lock);
     }
-    *output = *message;
-    pthread_cond_signal(pf->output_item);
+    // First END
+    *output = *input;
+    pthread_cond_signal(out_set->item);
     pthread_mutex_unlock(output_lock);
-    *message = CONSUMED;
-    pthread_cond_signal(pf->message_space);
-    pthread_mutex_unlock(message_lock);
-    next_message(&output, pf->output);
-    next_lock(&output_lock, pf->output_lock);
-    next_message(&message, pf->message);
-    next_lock(&message_lock, pf->message_lock);
+    *input = CONSUMED;
+    pthread_cond_signal(in_set->space);
+    pthread_mutex_unlock(input_lock);
+    output = out_set->buf + (output - out_set->buf + 1) % BUF_SIZE;
+    output_lock = out_set->lock + (output_lock - out_set->lock + 1) % BUF_SIZE;
+    input = in_set->buf + (input - in_set->buf + 1) % BUF_SIZE;
+    input_lock = in_set->lock + (input_lock - in_set->lock + 1) % BUF_SIZE;
+    // Buf elements
     while (buf != END) {
         pthread_mutex_lock(output_lock);
         if (*output != CONSUMED)
-            pthread_cond_wait(pf->output_space, output_lock);
-        pthread_mutex_lock(message_lock);
-        if (*message == CONSUMED)
-            pthread_cond_wait(pf->message_item, message_lock);
+            pthread_cond_wait(out_set->space, output_lock);
+        pthread_mutex_lock(input_lock);
+        if (*input == CONSUMED)
+            pthread_cond_wait(in_set->item, input_lock);
         *output = buf;
-        pthread_cond_signal(pf->output_item);
+        pthread_cond_signal(out_set->item);
         pthread_mutex_unlock(output_lock);
-        buf = *message;
-        *message = CONSUMED;
-        pthread_cond_signal(pf->message_space);
-        pthread_mutex_unlock(message_lock);
-        next_message(&output, pf->output);
-        next_lock(&output_lock, pf->output_lock);
-        next_message(&message, pf->message);
-        next_lock(&message_lock, pf->message_lock);
+        buf = *input;
+        *input = CONSUMED;
+        pthread_cond_signal(in_set->space);
+        pthread_mutex_unlock(input_lock);
+        output = out_set->buf + (output - out_set->buf + 1) % BUF_SIZE;
+        output_lock = out_set->lock + (output_lock - out_set->lock + 1) % BUF_SIZE;
+        input = in_set->buf + (input - in_set->buf + 1) % BUF_SIZE;
+        input_lock = in_set->lock + (input_lock - in_set->lock + 1) % BUF_SIZE;
     }
+    // Second END
     pthread_mutex_lock(output_lock);
     if (*output != CONSUMED)
-        pthread_cond_wait(pf->output_space, output_lock);
+        pthread_cond_wait(out_set->space, output_lock);
     *output = buf;
-    pthread_cond_signal(pf->output_item);
+    pthread_cond_signal(out_set->item);
     pthread_mutex_unlock(output_lock);
     return NULL;
 }
 
-static void *successor(void *output)
+static void *successor(void *buf_set)
 {
-    output_t *op = output;
-    int *message = op->message;
-    pthread_mutex_t *message_lock = op->message_lock;
-    pthread_mutex_lock(message_lock);
-    if (*message == CONSUMED)
-        pthread_cond_wait(op->message_item, message_lock);
-    *message = CONSUMED;
-    pthread_cond_signal(op->message_space);
-    pthread_mutex_unlock(message_lock);
-    next_message(&message, op->message);
-    next_lock(&message_lock, op->message_lock);
+    buf_set_t *b_set = buf_set;
+    int *buf = b_set->buf;
+    pthread_mutex_t *lock = b_set->lock;
+    pthread_mutex_lock(lock);
+    if (*buf == CONSUMED)
+        pthread_cond_wait(b_set->item, lock);
+    *buf = CONSUMED;
+    pthread_cond_signal(b_set->space);
+    pthread_mutex_unlock(lock);
+    buf = b_set->buf + (buf - b_set->buf + 1) % BUF_SIZE;
+    lock = b_set->lock + (lock - b_set->lock + 1) % BUF_SIZE;
     for (;;) {
-        pthread_mutex_lock(message_lock);
-        if (*message == CONSUMED)
-            pthread_cond_wait(op->message_item, message_lock);
-        if (*message == END) {
-            pthread_mutex_unlock(message_lock);
+        pthread_mutex_lock(lock);
+        if (*buf == CONSUMED)
+            pthread_cond_wait(b_set->item, lock);
+        if (*buf == END) {
+            pthread_mutex_unlock(lock);
             puts("");
             return NULL;
         }
-        printf("%d ", *message);
-        *message = CONSUMED;
-        pthread_cond_signal(op->message_space);
-        pthread_mutex_unlock(message_lock);
-        next_message(&message, op->message);
-        next_lock(&message_lock, op->message_lock);
+        printf("%d ", *buf);
+        *buf = CONSUMED;
+        pthread_cond_signal(b_set->space);
+        pthread_mutex_unlock(lock);
+        buf = b_set->buf + (buf - b_set->buf + 1) % BUF_SIZE;
+        lock = b_set->lock + (lock - b_set->lock + 1) % BUF_SIZE;
     }
 }
